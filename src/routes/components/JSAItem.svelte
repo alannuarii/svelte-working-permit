1a481b9de53ef6952cf2e841fa68345e558de9b8
alannuarii <alannuariii@gmail.com>
2023-01-20 18:48:57 +0800

    Update

--- src/routes/components/JSAItem.svelte
+++ src/routes/components/JSAItem.svelte
@@ -1,18 +1,46 @@
 <script>
-export let index
+	export let index;
+	export let tahap;
+	export let potensi;
+	export let pengendalian;
 </script>
 
-<div class="row">
-    <div class="col-lg-1 d-flex align-self-center justify-content-center">
-        <h6 class="text-center">1</h6>
-    </div>
-    <div class="col-lg-3">
-        <input type="text" class="form-control"  />
-    </div>
-    <div class="col-lg-3">
-        <input type="text" class="form-control" />
-    </div>
-    <div class="col-lg-5">
-        <input type="text" class="form-control" />
-    </div>
+<div class="row mb-2">
+	<div class="col-lg-1 d-flex align-self-center justify-content-center">
+		<h6 class="text-center">{index}</h6>
+	</div>
+	<div class="col-lg-3">
+		<input type="text" class="form-control" bind:value={tahap} />
+	</div>
+	<div class="col-lg-3">
+		<input type="text" class="form-control" bind:value={potensi} />
+	</div>
+	<div class="col-lg-4">
+		<input type="text" class="form-control" bind:value={pengendalian} />
+	</div>
+	<div class="col-lg-1 text-center">
+		<div class="btn"><i class="bi-dash-circle-fill text-danger" /></div>
+	</div>
 </div>
+
+<style>
+	h6 {
+		font-size: 15px;
+		margin-bottom: 0px;
+	}
+	input {
+		font-size: 12px;
+	}
+	.btn {
+		background: none;
+		color: inherit;
+		border: none;
+		padding: 0;
+		font: inherit;
+		cursor: pointer;
+		outline: inherit;
+	}
+	i {
+		font-size: 20px;
+	}
+</style>
--- src/routes/input/jsa/+page.svelte
+++ src/routes/input/jsa/+page.svelte
@@ -1,9 +1,23 @@
 <script>
 	import PageTitle from '../../components/PageTitle.svelte';
+	import JSAItem from '../../components/JSAItem.svelte';
+
+	let items = [];
+	let newItem = {
+		langkah: '',
+		potensi: '',
+		pengendalian: ''
+	};
+
+	const addItem = () => {
+		// items = [...items, newItem];
+		items.push(newItem);
+		items = [...items];
+	};
 </script>
 
 <section class="container mt-3">
-	<PageTitle title='buat jsa' />
+	<PageTitle title="buat jsa" />
 	<div class="card py-3 px-lg-5 px-4 rounded-0 shadow border-0">
 		<div class="row text-center">
 			<div class="col-lg-1">
@@ -15,37 +29,25 @@
 			<div class="col-lg-3">
 				<h5 class="text-primary-emphasis">Potensi Risiko</h5>
 			</div>
-			<div class="col-lg-5">
+			<div class="col-lg-4">
 				<h5 class="text-primary-emphasis">Pengendalian Risiko</h5>
 			</div>
 		</div>
 		<hr />
-		<!-- <div class="row">
-			<div class="col-lg-1 d-flex align-self-center justify-content-center">
-				<h6 class="text-center">1</h6>
+		{#each items as item, index}
+			<JSAItem {item} index={index + 1} />
+		{/each}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div class="d-flex justify-content-center mt-3">
+			<div class="btn btn-primary rounded-0" on:click={addItem}>
+				<i class="bi-plus-lg" /> Tambah
 			</div>
-			<div class="col-lg-3">
-				<input type="text" class="form-control" />
-			</div>
-			<div class="col-lg-3">
-				<input type="text" class="form-control" />
-			</div>
-			<div class="col-lg-5">
-				<input type="text" class="form-control" />
-			</div>
-		</div> -->
+		</div>
 	</div>
 </section>
 
 <style>
 	h5 {
 		font-size: 17px;
-	}
-	h6 {
-		font-size: 15px;
-		margin-bottom: 0px;
-	}
-	input {
-		font-size: 12px;
 	}
 </style>
